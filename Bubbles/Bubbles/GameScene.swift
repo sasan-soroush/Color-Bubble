288ea16d1b84e8b6feadc762246fbb92ec55623d
sasan <sasan.soroush21@gmail.com>
2019-07-14 13:13:06 +0430

    deleting balls by touch

--- Bubbles/Bubbles/GameScene.swift
+++ Bubbles/Bubbles/GameScene.swift
@@ -16,6 +16,7 @@ class GameScene: SKScene {
     var balls = ["ballBlue" , "ballGreen" , "ballRed" , "ballPurple" , "ballYellow" ]
     var motionManager : CMMotionManager?
     var scoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    var matchedBalls = Set<Ball>()
     
     var score = 0 {
         didSet {
@@ -36,7 +37,7 @@ class GameScene: SKScene {
         
         scoreLabel.fontSize = 35
         scoreLabel.text = "SCORE : 0"
-        scoreLabel.position = CGPoint(x: 20, y: 20)
+        scoreLabel.position = CGPoint(x: 10, y: 10)
         scoreLabel.horizontalAlignmentMode = .left
         scoreLabel.zPosition = 100
         addChild(scoreLabel)
@@ -68,14 +69,62 @@ class GameScene: SKScene {
         motionManager = CMMotionManager()
         motionManager?.startAccelerometerUpdates()
         
-        
-        
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         if let data = motionManager?.accelerometerData {
             physicsWorld.gravity = CGVector(dx: data.acceleration.y * -50, dy: data.acceleration.x * 50)
         }
     }
+    
+//    func getMatches(from node : Ball) {
+//        for body in node.physicsBody!.allContactedBodies() {
+//            guard let ball = body.node as? Ball else {continue}
+//            guard ball.name == node.name else {continue}
+//
+//            if !matchedBalls.contains(ball) {
+//                matchedBalls.insert(ball)
+//                getMatches(from: ball)
+//            }
+//        }
+//    }
+    
+    func getMatches(from startBall : Ball) {
+        let matchWidth = startBall.frame.width * startBall.frame.width * 1.1
+        
+        for node in children {
+            guard let ball = node as? Ball else {continue}
+            guard ball.name == startBall.name else {continue}
+            
+            let dist = distance(from: startBall, to: ball)
+            
+            guard dist < matchWidth else {continue}
+            
+            if !matchedBalls.contains(ball) {
+                matchedBalls.insert(ball)
+                getMatches(from: ball)
+            }
+            
+        }
+        
+    }
+    
+    func distance(from : Ball, to : Ball) -> CGFloat {
+        return (from.position.x - to.position.x) * (from.position.x - to.position.x) + (from.position.y - to.position.y) * (from.position.y - to.position.y)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        guard let postion = touches.first?.location(in: self) else {return}
+        guard let tappedBall = nodes(at: postion).first(where: {$0 is Ball}) as? Ball else {return}
+        
+        matchedBalls.removeAll(keepingCapacity: true)
+        
+        getMatches(from: tappedBall)
+        
+        if matchedBalls.count >= 3 {
+            score += Int(pow(2, Double(2)))
+            matchedBalls.forEach {$0.removeFromParent()}
+        }
+     }
 }
